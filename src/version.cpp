c3caeb82bc4951dc0ddf414d7de84f8ec292aed1
typhoonsimon <38272969+typhoonsimon@users.noreply.github.com>
2018-05-10 10:49:18 +0800

    3.2.0.1-alpha intermediate update

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("LitecoinPlus");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-devel"
+#define CLIENT_VERSION_SUFFIX   "-alpha"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
