30d6eeb678e1332f683957f5795644247f2ecfa2
typhoonsimon <38272969+typhoonsimon@users.noreply.github.com>
2018-06-14 17:54:23 +0800

    Changed another TRY_LOCK

--- src/net.cpp
+++ src/net.cpp
@@ -303,10 +303,23 @@ bool RecvLine(SOCKET hSocket, string& strLine)
 
 int GetnScore(const CService& addr)
 {
-    LOCK(cs_mapLocalHost);
-    if (mapLocalHost.count(addr) == LOCAL_NONE)
-        return 0;
-    return mapLocalHost[addr].nScore;
+    loop
+	{
+		{
+			TRY_LOCK(cs_mapLocalHost, lockMLH);
+			if (lockMLH)
+			{
+				if (mapLocalHost.count(addr) == LOCAL_NONE)
+					return 0;
+				return mapLocalHost[addr].nScore;
+			}
+			else
+			{
+				Sleep(20);
+				continue;
+			}
+		}
+	}
 }
 
 // Is our peer's addrLocal potentially useful as an external IP source?
@@ -1729,12 +1742,8 @@ void ThreadOpenConnections2(void* parg)
             addrConnect = addr;
             break;
         }
-
         if (addrConnect.IsValid())
-
-			// by Simone: if fail, let's break this loop and run the external one once again.
-            if (!OpenNetworkConnection(addrConnect, &grant))
-				break;
+			OpenNetworkConnection(addrConnect, &grant);
 		printf("OpenNetworkConnection() called by ThreadOpenConnections2() at the end of function\n");
     }
 }
