36fa050d204dc962aec01b7e64becea1a0460ab2
typhoonsimon <38272969+typhoonsimon@users.noreply.github.com>
2019-03-14 13:35:47 +0800

    Add files via upload

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -1202,6 +1202,7 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "sendalert"              && n > 3) ConvertTo<boost::int64_t>(params[3]);
     if (strMethod == "sendalert"              && n > 4) ConvertTo<boost::int64_t>(params[4]);
     if (strMethod == "sendalert"              && n > 5) ConvertTo<boost::int64_t>(params[5]);
+    if (strMethod == "sendalert"              && n > 6) ConvertTo<boost::int64_t>(params[6]);
 
     return params;
 }
