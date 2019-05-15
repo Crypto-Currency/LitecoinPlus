134d00ac68d492bf76d7912e21813ca919118922
Spidersbox <Spider@Spidersbox.com>
2019-05-15 14:39:58 -0700

    repairing arm build

--- src/scrypt_mine.cpp
+++ src/scrypt_mine.cpp
@@ -29,7 +29,15 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include <xmmintrin.h>
+
+extern "C"
+{
+  #ifndef NOSSE
+    #ifndef NO_ASM
+      #include <xmmintrin.h>
+    #endif
+  #endif
+}
 
 #include "scrypt_mine.h"
 #include "pbkdf2.h"
